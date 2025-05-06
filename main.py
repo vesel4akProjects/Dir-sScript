f7042698b06de91fe25c824a057d11756eccc46e
Vesel4ak <gushchinkv@yandex.ru>
2025-05-06 18:14:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from pyautogui import write , press
 import os
 from time import  sleep
+import psutil
 
 def get_scan():
     os.system("start cmd")
-    sleep(1)
-    write("color 2")
-    press("enter")
-    sleep(1)
-    write("dir /s")
-    press("enter")
 
 
+    for proc in psutil.process_iter(["name"]):
+        if proc.info["name"] == "cmd.exe":
+
+            sleep(3)
+            write("color 2")
+            press("enter")
+            sleep(1)
+            write("dir /s")
+            press("enter")
 
 if __name__ == "__main__":
     get_scan()
